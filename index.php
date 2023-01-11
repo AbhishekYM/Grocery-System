f206b76ec947d464451cd529aed88d0f22452db1
Abhishek Makwana <makwanaabhishek843@gmail.com>
2023-01-11 18:23:20 +0530

    add categories and products

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
 	<!--Code for linking css file-->
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<!--Code for linking css file-->
-
+	<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 	<link rel="icon" type="image/x-icon" href="/images/favicon.ico">
-	
+
 </head>
 
 <body>
@@ -75,20 +75,183 @@
 			<div class="total"> total : 300/-</div>
 			<a href="#" class="btn" id="cart-btn">Checkout</a>
 		</div>
-			<form action="" class="login-form">
-				<h3>Login now</h3>
-				<input type="email" class="box" placeholder=" your email">
-				<input type="password" class="box" placeholder=" your password">
-				<p>Forget Your password <a href="">Click Here</a></p>
-				<p>Dont Have An Account <a href="">Create Now</a></p>
+		<form action="" class="login-form">
+			<h3>Login now</h3>
+			<input type="email" class="box" placeholder=" your email">
+			<input type="password" class="box" placeholder=" your password">
+			<p>Forget Your password <a href="">Click Here</a></p>
+			<p>Dont Have An Account <a href="">Create Now</a></p>
 
-				<input type="submit" value="Login Now" class="btn ">
-			</form>
-		
+			<input type="submit" value="Login Now" class="btn ">
+		</form>
 
 	</header>
 	<!--Header Section-->
 
+	<!--Banner Section-->
+	<div class="home" id="home">
+		<section class="content">
+			<h3>Fresh And <span> Organic </span>Products For You</h3>
+			<p>An online grocer is either a brick-and-mortar supermarket or grocery store that allows online ordering,
+				or a standalone e-commerce service that includes grocery items. There is usually a delivery charge for
+				this service.</p>
+			<a href="#" class="btn">shop now</a>
+	</div>
+
+	</section>
+	<!--Banner Section-->
+
+	<!--Feature Section-->
+	<section class="features" id="features">
+		<h1 class="heading"> our <span>features</span> </h1>
+		<div class="box-container">
+			<div class="box">
+				<img src="image/image/feature-img-1.png" alt="" srcset="">
+				<h3>fresh and organic</h3>
+				<p>Our website provides fresh and hygenic grocery items.We provide organic items to provide best quality products to the customer</p>
+				<a href="#" class="btn">read more</a>
+			</div>
+			<div class="box">
+				<img src="image/image/feature-img-2.png" alt="" srcset="">
+				<h3>free delivery</h3>
+				<p>Our website provides free delivery on all the products min(RS.500 and above).Our website provides one day delivery to the plus customer member.</p>
+				<a href="#" class="btn">read more</a>
+			</div>
+			<div class="box">
+				<img src="image/image/feature-img-3.png" alt="" srcset="">
+				<h3>Easy Payments</h3>
+				<p>Our website provides all the methods of payment i.e UPI payment, Credit card, Debit card.Our website uses end to end encription in storing details.</p>
+				<a href="#" class="btn">read more</a>
+			</div>
+		</div>
+
+	</section>
+	<!--Feature Section-->
+
+	<!--Products Section-->
+	<section class="products" id="products">
+		<h1 class="heading"> our <span>products</span> </h1>
+
+		<div class="swiper product-slider">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide box">
+					<img src="image/image/product-1.png" alt="">
+					<h1>fresh orange</h1>
+					<div class="price">100/- - 200/-</div>
+					<div class="stars">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-half"></i>
+					</div>
+					<a href="" class="btn">add to cart</a>
+				</div>
+				<div class="swiper-slide box">
+					<img src="image/image/product-2.png" alt="">
+					<h1>fresh onion</h1>
+					<div class="price">100/- - 200/-</div>
+					<div class="stars">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-half"></i>
+					</div>
+					<a href="" class="btn">add to cart</a>
+				</div>
+				<div class="swiper-slide box">
+					<img src="image/image/product-3.png" alt="">
+					<h1>fresh meat</h1>
+					<div class="price">100/- - 200/-</div>
+					<div class="stars">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-half"></i>
+					</div>
+					<a href="" class="btn">add to cart</a>
+				</div>
+				<div class="swiper-slide box">
+					<img src="image/image/product-4.png" alt="">
+					<h1>fresh cabbage</h1>
+					<div class="price">100/- - 200/-</div>
+					<div class="stars">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-half"></i>
+					</div>
+					<a href="" class="btn">add to cart</a>
+				</div>
+			</div>
+		</div>
+
+
+
+		<div class="swiper product-slider">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide box">
+					<img src="image/image/product-5.png" alt="">
+					<h1>fresh potato</h1>
+					<div class="price">100/- - 200/-</div>
+					<div class="stars">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-half"></i>
+					</div>
+					<a href="" class="btn">add to cart</a>
+				</div>
+				<div class="swiper-slide box">
+					<img src="image/image/product-6.png" alt="">
+					<h1>fresh Guvava</h1>
+					<div class="price">100/- - 200/-</div>
+					<div class="stars">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-half"></i>
+					</div>
+					<a href="" class="btn">add to cart</a>
+				</div>
+				<div class="swiper-slide box">
+					<img src="image/image/product-7.png" alt="">
+					<h1>fresh carrot</h1>
+					<div class="price">100/- - 200/-</div>
+					<div class="stars">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-half"></i>
+					</div>
+					<a href="" class="btn">add to cart</a>
+				</div>
+				<div class="swiper-slide box">
+					<img src="image/image/product-8.png" alt="">
+					<h1>fresh lemon</h1>
+					<div class="price">100/- - 200/-</div>
+					<div class="stars">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-half"></i>
+					</div>
+					<a href="" class="btn">add to cart</a>
+				</div>
+				
+			</div>
+			<div class="swiper-pagination"></div>
+					</div>
+	</section>
+	<!--Products Section-->
+	<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 	<script src="js/script.js"></script>
 
 </body>
--- js/script.js
+++ js/script.js
@@ -1,29 +1,57 @@
 let searchForm = document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick = () =>
-{
+document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
-   
-} 
+    shoppingCart.classList.remove('active');
+    loginform.classList.remove('active');
+}
 
- let shoppingCart = document.querySelector('.shopping-cart');
-document.querySelector('#cart-btn').onclick = () =>
-{
-   
-     shoppingCart.classList.toggle('active');
-     
+let shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick = () => {
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginform.classList.remove('active');
+
 }
 
 let loginform = document.querySelector('.login-form');
-document.querySelector('#login-btn').onclick = () =>
-{
-    
-    
+document.querySelector('#login-btn').onclick = () => {
+
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
     loginform.classList.toggle('active');
-     
 }
 
 let navbar = document.querySelector('.nav-bar');
-document.querySelector('#menu-btn').onclick = () =>
-{   
+document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 }
+
+
+var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+
+        },
+        768: {
+            slidesPerView: 2,
+
+        },
+        1020: {
+            slidesPerView: 3,
+
+        },
+
+    },
+});
